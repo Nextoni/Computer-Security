b2e56cdfd3e4c83ffa1261f6b067e5f54bbdd444
Nextoni <138438390+Nextoni@users.noreply.github.com>
2024-03-10 11:11:38 +0200

    Create Ceaser_encrypt_file.c

---
+++ Ceaser_encrypt_file.c
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+const int MAXN = 1000;
+    
+char* caesar_encrypt(char* cypher, int key)
+{
+    int length = strlen(cypher);
+    char* str = (char*) malloc(sizeof(char) * (length + 1));
+    
+    int i = 0;
+    while(cypher[i] != '\0')
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            str[i] = ((cypher[i] - 'A' + key + 26) % 26) + 'A';
+        }
+        else if(str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = ((cypher[i] - 'a' + key + 26) % 26) + 'a';
+        }
+        else if(str[i] >= '0' && str[i] <= '9'){
+            str[i] = ((cypher[i] - '0' + key + 10) % 10) + '0';
+        }
+        
+        i++;
+    }
+    str[i] = '\0';
+    
+    return str;
+}
+
+
+char* caesar_decrypt(char* cypher, int key){
+    int length = strlen(cypher);
+    char* str = (char*) malloc(sizeof(char) * (length + 1));
+    
+    int i = 0;
+    while(cypher[i] != '\0')
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            str[i] = ((cypher[i] - 'A' - key + 26) % 26) + 'A';
+        }
+        else if(str[i] >= 'a' && str[i] <= 'z'){
+            str[i] = ((cypher[i] - 'a' - key + 26) % 26) + 'a';
+        }
+        else if(str[i] >= '0' && str[i] <= '9'){
+            str[i] = ((cypher[i] - '0' - key + 10) % 10) + '0';
+        }
+        
+        i++;
+    }
+    str[i] = '\0';
+    
+    return str;
+}
+
+
+int read_cypher(char* filename, char* cypher)
+{
+    FILE *file;
+    
+    file = fopen(filename, "r");
+    
+    if(file == NULL)
+    {
+        printf("Error opening the file.");
+        
+        return EXIT_FAILURE;
+    }
+    
+    if(fgets(cypher, MAXN, file) == NULL)
+    {
+        printf("Error reading from file.");
+        
+        return EXIT_FAILURE;
+    }
+    
+    fclose(file);
+    
+    return EXIT_SUCCESS;
+}
+
+int main() {
+    char filename[MAXN];
+    printf("Enter filename: \n");
+    scanf("%s", filename);
+    
+
+    char cypher[MAXN];
+
+    if( read_cypher(filename, cypher) == EXIT_SUCCESS)
+    {
+        int key;
+        printf("Enter key:");
+        scanf("%d", &key);
+        
+        char* result = caesar_encrypt(cypher, key);
+        
+        printf("%s", result);
+        
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+}
